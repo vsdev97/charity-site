5a503d65df64da9c197cefb3ab6d39bb15a8702c
VenuMadhav Sheshabhattar <vsheshabhattar210@everestdx.com>
2024-12-06 01:15:01 +0530

    handled routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, Link } from "react-router-dom";
 import About from "./pages/about";
 import Home from "./pages/home";
 
